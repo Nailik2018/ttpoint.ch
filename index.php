545967fc89eade6ce53500191c2cbd80fd3a150e
SURFACE-BOOK-KI\kilua <kilualthaus@hotmail.com>
2020-11-05 15:36:08 +0100

    navigation projektaufbau, bootstrrap

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php require_once "./v1/assets/header.php" ?>
+    <link rel="stylesheet" href="/v1/css/index.css">
+    <title>Startseite ttpoint.ch</title>
+<?php require_once "./v1/assets/navigation.php" ?>
+
+    <br />
+    <br />
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+
+    <script type="text/javascript" src="v1/js/index.js"></script>
+
+<?php require_once "./v1/assets/footer.php" ?>
---
+++ v1/assets/navigation.php
@@ -0,0 +1,22 @@
+<script type="text/javascript" src="v1/js/navigation.js"></script>
+<title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="#"><img src="v1/assets/logos/table-tennis1.svg"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapsed" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <!--<utton class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">-->
+        <span class="navbar-toggler-icon" onclick="showMenu()"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="https://ttpoint.ch/v1/verbaende">Verbände<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="https://ttpoint.ch/v1/ranking">Rankings<span class="sr-only"></span></a>
+            </li>
+        </ul>
+        <span class="navbar-text">
+    </span>
+    </div>
+</nav>
+</nav>
